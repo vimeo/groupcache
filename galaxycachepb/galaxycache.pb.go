b601793164de6bcf70629785898c0dff3bb19590
Will Greenberg <hello@will-greenberg.com>
2019-06-25 16:29:09 -0400

    Change pb service name Galaxycache to Peer

--- galaxycachepb/galaxycache.pb.go
+++ galaxycachepb/galaxycache.pb.go
@@ -6,9 +6,10 @@ package galaxycachepb
 import (
 	context "context"
 	fmt "fmt"
+	math "math"
+
 	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
-	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -124,7 +125,7 @@ func init() {
 func init() { proto.RegisterFile("galaxycache.proto", fileDescriptor_23bd509ca7b74957) }
 
 var fileDescriptor_23bd509ca7b74957 = []byte{
-	// 187 bytes of a gzipped FileDescriptorProto
+	// 180 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4c, 0x4f, 0xcc, 0x49,
 	0xac, 0xa8, 0x4c, 0x4e, 0x4c, 0xce, 0x48, 0xd5, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x45,
 	0x12, 0x2a, 0x48, 0x52, 0x32, 0xe3, 0xe2, 0x72, 0x4f, 0x2d, 0x09, 0x4a, 0x2d, 0x2c, 0x4d, 0x2d,
@@ -133,10 +134,10 @@ var fileDescriptor_23bd509ca7b74957 = []byte{
 	0x1b, 0xac, 0xaf, 0xb8, 0x20, 0x3f, 0xaf, 0x38, 0x55, 0x48, 0x84, 0x8b, 0xb5, 0x2c, 0x31, 0xa7,
 	0x34, 0x15, 0xac, 0x8f, 0x27, 0x08, 0xc2, 0x11, 0x92, 0xe5, 0xe2, 0xca, 0xcd, 0xcc, 0x2b, 0x2d,
 	0x49, 0x8d, 0x2f, 0x2c, 0x28, 0x06, 0xeb, 0x66, 0x0c, 0xe2, 0x84, 0x88, 0x04, 0x16, 0x14, 0x1b,
-	0x85, 0x73, 0x71, 0xbb, 0x23, 0x1c, 0x23, 0xe4, 0xc1, 0xc5, 0xeb, 0x9e, 0x5a, 0xe2, 0x56, 0x94,
-	0x9f, 0x1b, 0x94, 0x9a, 0x9b, 0x5f, 0x92, 0x2a, 0x24, 0xa9, 0x87, 0xe2, 0x56, 0x3d, 0x84, 0x43,
-	0xa5, 0xa4, 0xb0, 0x49, 0x41, 0xdc, 0xa2, 0xc4, 0x90, 0xc4, 0x06, 0xf6, 0xaa, 0x31, 0x20, 0x00,
-	0x00, 0xff, 0xff, 0x6c, 0x17, 0xf8, 0xc6, 0xff, 0x00, 0x00, 0x00,
+	0xf9, 0x71, 0xb1, 0x04, 0xa4, 0xa6, 0x16, 0x09, 0xb9, 0x81, 0xcd, 0x72, 0x2b, 0xca, 0xcf, 0x05,
+	0x73, 0x25, 0xf5, 0x50, 0x9c, 0xa8, 0x87, 0x70, 0x9f, 0x94, 0x14, 0x36, 0x29, 0x88, 0x13, 0x94,
+	0x18, 0x92, 0xd8, 0xc0, 0x3e, 0x34, 0x06, 0x04, 0x00, 0x00, 0xff, 0xff, 0x52, 0x24, 0xf6, 0xcd,
+	0xf6, 0x00, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -147,64 +148,64 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// GalaxycacheClient is the client API for Galaxycache service.
+// PeerClient is the client API for Peer service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type GalaxycacheClient interface {
-	GetFromRemote(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error)
+type PeerClient interface {
+	GetFromPeer(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error)
 }
 
-type galaxycacheClient struct {
+type peerClient struct {
 	cc *grpc.ClientConn
 }
 
-func NewGalaxycacheClient(cc *grpc.ClientConn) GalaxycacheClient {
-	return &galaxycacheClient{cc}
+func NewPeerClient(cc *grpc.ClientConn) PeerClient {
+	return &peerClient{cc}
 }
 
-func (c *galaxycacheClient) GetFromRemote(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error) {
+func (c *peerClient) GetFromPeer(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*GetResponse, error) {
 	out := new(GetResponse)
-	err := c.cc.Invoke(ctx, "/galaxycachepb.Galaxycache/GetFromRemote", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/galaxycachepb.Peer/GetFromPeer", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// GalaxycacheServer is the server API for Galaxycache service.
-type GalaxycacheServer interface {
-	GetFromRemote(context.Context, *GetRequest) (*GetResponse, error)
+// PeerServer is the server API for Peer service.
+type PeerServer interface {
+	GetFromPeer(context.Context, *GetRequest) (*GetResponse, error)
 }
 
-func RegisterGalaxycacheServer(s *grpc.Server, srv GalaxycacheServer) {
-	s.RegisterService(&_Galaxycache_serviceDesc, srv)
+func RegisterPeerServer(s *grpc.Server, srv PeerServer) {
+	s.RegisterService(&_Peer_serviceDesc, srv)
 }
 
-func _Galaxycache_GetFromRemote_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Peer_GetFromPeer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(GetRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GalaxycacheServer).GetFromRemote(ctx, in)
+		return srv.(PeerServer).GetFromPeer(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/galaxycachepb.Galaxycache/GetFromRemote",
+		FullMethod: "/galaxycachepb.Peer/GetFromPeer",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GalaxycacheServer).GetFromRemote(ctx, req.(*GetRequest))
+		return srv.(PeerServer).GetFromPeer(ctx, req.(*GetRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _Galaxycache_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "galaxycachepb.Galaxycache",
-	HandlerType: (*GalaxycacheServer)(nil),
+var _Peer_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "galaxycachepb.Peer",
+	HandlerType: (*PeerServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "GetFromRemote",
-			Handler:    _Galaxycache_GetFromRemote_Handler,
+			MethodName: "GetFromPeer",
+			Handler:    _Peer_GetFromPeer_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
