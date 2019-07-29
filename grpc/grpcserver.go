e715557e552fdf37a04219d3cba55eeff86ed802
Will Greenberg <hello@will-greenberg.com>
2019-07-29 17:50:15 -0400

    grpcserver.go: remove adaption note

--- grpc/grpcserver.go
+++ grpc/grpcserver.go
@@ -1,6 +1,5 @@
 /*
  Copyright 2019 Vimeo Inc.
- Adapted from https://github.com/charithe/gcgrpcpool/blob/master/gcgrpcpool.go
 
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
