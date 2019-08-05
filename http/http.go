32873ea65fbf76ce1df745702c96eb9be5a4bec6
Will Greenberg <hello@will-greenberg.com>
2019-08-05 18:05:01 -0400

    http.go: update copyright

--- http/http.go
+++ http/http.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2019 Vimeo Inc.
+Copyright 2013 Google Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
