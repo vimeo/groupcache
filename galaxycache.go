eb212992fde9f550df6b1e27677271a9bf244464
David Finkel <davidf@vimeo.com>
2019-12-09 15:03:05 -0500

    Plumb cache-hit-level information up the callstack
    Due to single-flighting some apsects of tracing are a bit more difficult
    than we'd usually expect, so plumb back information about
    cache-hit-levels in the results struct.

--- galaxycache.go
+++ galaxycache.go
@@ -340,7 +340,9 @@ func (h hitLevel) isHit() bool {
 
 // recordRequest records the corresponding opencensus measurement
 // to the level at which data was found on Get/load
-func (g *Galaxy) recordRequest(ctx context.Context, h hitLevel) {
+func (g *Galaxy) recordRequest(ctx context.Context, h hitLevel, localAuthoritative bool) {
+	span := trace.FromContext(ctx)
+	span.Annotatef([]trace.Attribute{trace.StringAttribute("hit_level", h.String())}, "fetched from %s", h)
 	switch h {
 	case hitMaincache:
 		g.Stats.MaincacheHits.Add(1)
@@ -354,6 +356,9 @@ func (g *Galaxy) recordRequest(ctx context.Context, h hitLevel) {
 	case hitBackend:
 		g.Stats.BackendLoads.Add(1)
 		stats.Record(ctx, MBackendLoads.M(1))
+		if !localAuthoritative {
+			span.Annotatef(nil, "failed to fetch from peer, not authoritative for key")
+		}
 	}
 }
 
@@ -389,14 +394,14 @@ func (g *Galaxy) Get(ctx context.Context, key string, dest Codec) error {
 	stats.Record(ctx, MKeyLength.M(int64(len(key))))
 
 	if hlvl.isHit() {
-		span.Annotatef(nil, "Cache hit")
+		span.Annotatef([]trace.Attribute{trace.BoolAttribute("cache_hit", true)}, "Cache hit in %s", hlvl)
 		value.stats.touch()
-		g.recordRequest(ctx, hlvl)
+		g.recordRequest(ctx, hlvl, false)
 		stats.Record(ctx, MValueLength.M(int64(len(value.data))))
 		return dest.UnmarshalBinary(value.data)
 	}
 
-	span.Annotatef(nil, "Cache miss")
+	span.Annotatef([]trace.Attribute{trace.BoolAttribute("cache_hit", false)}, "Cache miss")
 	// Optimization to avoid double unmarshalling or copying: keep
 	// track of whether the dest was already populated. One caller
 	// (if local) will set this; the losers will not. The common
@@ -417,8 +422,11 @@ func (g *Galaxy) Get(ctx context.Context, key string, dest Codec) error {
 }
 
 type valWithLevel struct {
-	val   *valWithStat
-	level hitLevel
+	val                *valWithStat
+	level              hitLevel
+	localAuthoritative bool
+	peerErr            error
+	localErr           error
 }
 
 // load loads key either by invoking the getter locally or by sending it to another machine.
@@ -455,19 +463,21 @@ func (g *Galaxy) load(ctx context.Context, key string, dest Codec) (value *valWi
 				g.Stats.CoalescedMaincacheHits.Add(1)
 			}
 			stats.RecordWithTags(ctx, []tag.Mutator{tag.Insert(CacheLevelKey, hlvl.String())}, MCoalescedCacheHits.M(1))
-			return &valWithLevel{value, hlvl}, nil
+			return &valWithLevel{value, hlvl, false, nil, nil}, nil
 
 		}
 		g.Stats.CoalescedLoads.Add(1)
 		stats.Record(ctx, MCoalescedLoads.M(1))
 
-		var err error
+		authoritative := true
+		var peerErr error
 		if peer, ok := g.peerPicker.pickPeer(key); ok {
-			value, err = g.getFromPeer(ctx, peer, key)
-			if err == nil {
+			value, peerErr = g.getFromPeer(ctx, peer, key)
+			authoritative = false
+			if peerErr == nil {
 				g.Stats.CoalescedPeerLoads.Add(1)
 				stats.Record(ctx, MCoalescedPeerLoads.M(1))
-				return &valWithLevel{value, hitPeer}, nil
+				return &valWithLevel{value, hitPeer, false, nil, nil}, nil
 			}
 
 			g.Stats.PeerLoadErrors.Add(1)
@@ -489,12 +499,13 @@ func (g *Galaxy) load(ctx context.Context, key string, dest Codec) (value *valWi
 		destPopulated = true // only one caller of load gets this return value
 		value = newValWithStat(data, nil)
 		g.populateCache(key, value, &g.mainCache)
-		return &valWithLevel{value, hitBackend}, nil
+		return &valWithLevel{value, hitBackend, authoritative, peerErr, err}, nil
 	})
 	if err == nil {
 		value = viewi.(*valWithLevel).val
 		level := viewi.(*valWithLevel).level
-		g.recordRequest(ctx, level) // record the hits for all load calls, including those that tagged onto the singleflight
+		authoritative := viewi.(*valWithLevel).localAuthoritative
+		g.recordRequest(ctx, level, authoritative) // record the hits for all load calls, including those that tagged onto the singleflight
 	}
 	return
 }
