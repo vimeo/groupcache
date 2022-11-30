3ac4e34a6410c40f27a0a365e01124e5b30b39d4
David Finkel <davidf@vimeo.com>
2022-11-29 19:49:47 -0500

    protocodec: introduce CodecV2
    google.golang.org/protobuf has been the current protobuf version for a
    while. Add an updated protocodec and move the test protobuf package to
    support it.
    
    Make the new CodecV2 type generic to eliminate the need to type-assert
    when dealing with a known-type.
    
    Include a NewV2 helper to make type-inference a bit nicer.

--- protocodec/codec_v2_test.go
+++ protocodec/codec_v2_test.go
@@ -1,11 +1,13 @@
+//go:build go1.18
+
 /*
-Copyright 2019 Vimeo Inc.
+Copyright 2022 Vimeo Inc.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-     http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,23 +16,22 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package protocodec
+package protocodec_test
 
 import (
 	"testing"
 
-	"github.com/golang/protobuf/proto"
-
-	"github.com/vimeo/galaxycache/protocodec/internal/testpb"
+	"github.com/vimeo/galaxycache/protocodec"
+	"github.com/vimeo/galaxycache/protocodec/internal/testpbv2"
+	"google.golang.org/protobuf/proto"
 )
 
-func TestProtoCodec(t *testing.T) {
-	inProtoCodec := &ProtoCodec{
-		Msg: &testpb.TestMessage{
-			Name: proto.String("TestName"),
-			City: proto.String("TestCity"),
-		},
-	}
+func TestProtoCodecV2(t *testing.T) {
+	inProtoCodec := protocodec.NewV2[testpbv2.TestMessage]()
+	inProtoCodec.Set(&testpbv2.TestMessage{
+		Name: proto.String("TestName"),
+		City: proto.String("TestCity"),
+	})
 
 	testMsgBytes, err := inProtoCodec.MarshalBinary()
 	if err != nil {
@@ -38,16 +39,13 @@ func TestProtoCodec(t *testing.T) {
 	}
 	t.Logf("Marshaled Bytes: %q", string(testMsgBytes))
 
-	outProtoCodec := &ProtoCodec{
-		Msg: &testpb.TestMessage{},
-	}
+	outProtoCodec := protocodec.NewV2[testpbv2.TestMessage]()
 
 	if unmarshalErr := outProtoCodec.UnmarshalBinary(testMsgBytes); unmarshalErr != nil {
 		t.Errorf("Error unmarshaling: %s", unmarshalErr)
 	}
 
-	if !proto.Equal(outProtoCodec.Msg, inProtoCodec.Msg) {
-		t.Errorf("UnmarshalBinary resulted in %q; want %q", outProtoCodec.Msg.String(), inProtoCodec.Msg.String())
+	if !proto.Equal(outProtoCodec.Get(), inProtoCodec.Get()) {
+		t.Errorf("UnmarshalBinary resulted in %q; want %q", outProtoCodec.Get().String(), inProtoCodec.Get().String())
 	}
-
 }
