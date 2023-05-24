108ab0f5e3d3721480f07308c56b3ab7838f94ca
David Finkel <davidf@vimeo.com>
2023-05-24 16:07:26 -0400

    protocodec: add a convenience GalaxyGet method
    We may add a similar version to the top-level package later, but for
    now, type-inference leans in the direction of making it codec-specific.

---
+++ protocodec/galaxywrap_v2.go
@@ -0,0 +1,20 @@
+//go:build go1.18
+
+package protocodec
+
+import (
+	"context"
+
+	"github.com/vimeo/galaxycache"
+)
+
+// GalaxyGet is a simple wrapper around a Galaxy.Get method-call that takes
+// care of constructing the protocodec.CodecV2, etc. (making the interface more idomatic for Go)
+func GalaxyGet[C any, T pointerMessage[C]](ctx context.Context, g *galaxycache.Galaxy, key string) (T, error) {
+	pc := NewV2[C, T]()
+	getErr := g.Get(ctx, key, &pc)
+	if getErr != nil {
+		return nil, getErr
+	}
+	return pc.Get(), nil
+}
